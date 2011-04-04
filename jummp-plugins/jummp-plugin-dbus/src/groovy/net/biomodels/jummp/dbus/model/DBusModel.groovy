71054340faca3bb3fbf9087622435ac9eb24f4c1
Martin Gräßlin <m.graesslin@dkfz.de>
2011-04-04 13:42:25 +0200

    Serialize the ModelFormat name in DBusModel

--- jummp-plugins/jummp-plugin-dbus/src/groovy/net/biomodels/jummp/dbus/model/DBusModel.groovy
+++ jummp-plugins/jummp-plugin-dbus/src/groovy/net/biomodels/jummp/dbus/model/DBusModel.groovy
@@ -19,26 +19,30 @@ class DBusModel extends ModelTransportCommand implements DBusSerializable {
     public DBusModel() {}
 
     Object[] serialize() {
-        Object[] retVals = new Object[9]
+        Object[] retVals = new Object[10]
         retVals[0] = this.id ? this.id : 0
         retVals[1] = this.name ? this.name : ""
         retVals[2] = this.state ? this.state.toString() : ""
         retVals[3] = this.format ? this.format.identifier : ""
-        retVals[4] = this.comment ? this.comment : ""
-        retVals[5] = this.lastModifiedDate ? this.lastModifiedDate.getTime() : 0
-        retVals[6] = this.submitter ? this.submitter : ""
-        retVals[7] = this.submissionDate ? this.submissionDate.getTime() : 0
-        retVals[8] = this.creators ? this.creators.toArray() : [''].toArray()
+        retVals[4] = this.format ? this.format.name : ""
+        retVals[5] = this.comment ? this.comment : ""
+        retVals[6] = this.lastModifiedDate ? this.lastModifiedDate.getTime() : 0
+        retVals[7] = this.submitter ? this.submitter : ""
+        retVals[8] = this.submissionDate ? this.submissionDate.getTime() : 0
+        retVals[9] = this.creators ? this.creators.toArray() : [''].toArray()
         return retVals
     }
 
-    public void deserialize(long id, String name, String state, String format, String comment, long lastModifiedDate, String submitter, long submissionDate, String[] creators) {
+    public void deserialize(long id, String name, String state, String format, String formatName, String comment, long lastModifiedDate, String submitter, long submissionDate, String[] creators) {
         this.id = id
         this.name = name
         //this.state =
         this.comment = comment
         if (format != "") {
             this.format = new ModelFormatTransportCommand(identifier: format)
+            if (formatName != "") {
+                this.format.name = formatName
+            }
         }
         if (comment != "") {
             this.comment = comment
