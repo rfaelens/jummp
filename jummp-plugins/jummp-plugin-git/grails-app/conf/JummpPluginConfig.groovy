2ed923c1a32f694344cdbee9fd3dc6c296ac12cc
Martin Gräßlin <m.graesslin@dkfz.de>
2011-04-12 09:02:05 +0200

    Adjust pluginServiceName for gitMangerFactory
    Missed that one in the commit for GitManager.

--- jummp-plugins/jummp-plugin-git/grails-app/conf/JummpPluginConfig.groovy
+++ jummp-plugins/jummp-plugin-git/grails-app/conf/JummpPluginConfig.groovy
@@ -4,7 +4,7 @@ class JummpPluginConfig {
     static def configure = { ConfigObject jummp, ConfigObject jummpConfig ->
         if (jummpConfig.jummp.vcs.plugin == "git") {
             println("using git as vcs backend")
-            jummp.vcs.pluginServiceName = "gitService"
+            jummp.vcs.pluginServiceName = "gitManagerFactory"
             jummp.plugins.git.enabled = true
         }
     }
