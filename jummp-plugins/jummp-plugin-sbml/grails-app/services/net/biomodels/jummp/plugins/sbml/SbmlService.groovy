c4aa8813262aae1066baa571c9a3760e03cad262
Martin Gräßlin <m.graesslin@dkfz.de>
2011-05-11 14:18:23 +0200

    Begin SBML parsing fun
    SbmlService provides first methods to parse model information like
    level, version and metaId on model level. The service uses the new
    cache to not re-parse the file each time a model is accessed.
    
    A new integration test is added to core to test these new methods.
    The test cannot be in the plugin as we need access to core services
    as we need to access the underlying VCS to retrieve model files if
    not in the cache (this is of course protected by using the ModelService).

--- jummp-plugins/jummp-plugin-sbml/grails-app/services/net/biomodels/jummp/plugins/sbml/SbmlService.groovy
+++ jummp-plugins/jummp-plugin-sbml/grails-app/services/net/biomodels/jummp/plugins/sbml/SbmlService.groovy
@@ -5,17 +5,26 @@ import net.biomodels.jummp.core.model.FileFormatService
 import org.sbml.jsbml.SBMLDocument
 import org.sbml.jsbml.SBMLError
 import org.sbml.jsbml.SBMLReader
+import net.biomodels.jummp.core.model.RevisionTransportCommand
 
 /**
  * Service class for handling Model files in the SBML format.
  * @author  Martin Gräßlin <m.graesslin@dkfz-heidelberg.de>
  */
 class SbmlService implements FileFormatService {
-    // TODO: the service might need a cache of the already parsed SBMLDocuments
 
     static transactional = true
 
+    /**
+     * Dependency Injection for ModelDelegateService allowing to access models
+     */
+    def modelDelegateService
+
+    // TODO: move initialization into afterPropertiesSet and make it configuration dependent
+    SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache(100)
+
     public boolean validate(final File model) {
+        // TODO: we should insert the parsed model into the cache
         SBMLDocument doc
         SBMLReader reader = new SBMLReader()
         try {
@@ -48,5 +57,52 @@ class SbmlService implements FileFormatService {
 
     public String extractName(final File model) {
         return ""
+    }
+
+    /**
+     * Retrieves the metaId on the Model level. Does not retrieve the metaId of child elements
+     * of model such as compartment.
+     * @param revision
+     * @return MetaId on Model level
+     */
+    public String getMetaId(RevisionTransportCommand revision) {
+        return getFromCache(revision).model.metaId
+    }
+
+    /**
+     *
+     * @param revision
+     * @return Version of the SBML file
+     */
+    public long getVersion(RevisionTransportCommand revision) {
+        return getFromCache(revision).version
+    }
+
+    /**
+     *
+     * @param revision
+     * @return Level of the SBML file
+     */
+    public long getLevel(RevisionTransportCommand revision) {
+        return getFromCache(revision).level
+    }
+
+    /**
+     * Returns the SBMLDocument for the @p revision from the cache.
+     * If the cache does not contain the SBMLDocument, the model file is
+     * retrieved, parsed and inserted into the Cache.
+     * @param revision The revision for which the SBMLDocument needs to be retrieved
+     * @return The parsed SBMLDocument
+     */
+    private SBMLDocument getFromCache(RevisionTransportCommand revision) throws XMLStreamException {
+        SBMLDocument document = cache.get(revision)
+        if (document) {
+            return document
+        }
+        // we do not have a document, so retrieve first the file
+        byte[] bytes = modelDelegateService.retrieveModelFile(revision)
+        document = (new SBMLReader()).readSBMLFromStream(new ByteArrayInputStream(bytes))
+        cache.put(revision, document)
+        return document
     }
 }
