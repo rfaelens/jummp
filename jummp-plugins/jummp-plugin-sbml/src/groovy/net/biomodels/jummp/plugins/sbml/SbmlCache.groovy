1dc19d4581abb6aea5c3d8cffb751e82b7915f1d
Martin Gräßlin <m.graesslin@dkfz.de>
2011-05-11 10:34:10 +0200

    Adding a most recently used cache for SBMLDocuments
    The cache is a simple Map using a RevisionTransportCommand as the
    key and an SBMLDocument as the value. The cache is intended for
    keeping recently used SBML Models parsed instead of re-parsing
    each time it is required.
    
    The implementation is thread safe.
    
    Currently not all methods are properly tested yet.

---
+++ jummp-plugins/jummp-plugin-sbml/src/groovy/net/biomodels/jummp/plugins/sbml/SbmlCache.groovy
@@ -0,0 +1,208 @@
+package net.biomodels.jummp.plugins.sbml
+
+import org.sbml.jsbml.SBMLDocument
+import net.biomodels.jummp.core.model.RevisionTransportCommand
+import java.util.AbstractMap.SimpleEntry
+import java.util.concurrent.locks.ReentrantLock
+
+/**
+ * @short A last recently used cache for SBMLDocument.
+ *
+ * This class provides a last recently used cache for SBMLDocuments. Internally it uses a
+ * LinkedHashMap mapping ids to the SBMLDocument. It is meant to keep the SBML documents
+ * belonging to a specific Revision in memory. Because of that the actual class uses
+ * RevisionTransportCommands as the key.
+ *
+ * All access to the internal cache is protected by a reentrant lock, so that the cache can
+ * be accessed from multiple threads.
+ *
+ * @autor Martin Gräßlin <m.graesslin@dkfz.de> 
+ */
+class SbmlCache<K extends RevisionTransportCommand, V extends SBMLDocument> implements Map<K, V> {
+
+    /**
+     * Internal cache extending LinkedHashMap with the contract of a last recently used cache.
+     */
+    private class InternalCache<K extends Long, V extends SBMLDocument> extends LinkedHashMap<K, V> {
+        private Integer maxSize
+        InternalCache(int maxSize) {
+            super(0, 0.75, true)
+            this.maxSize = maxSize
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<? super Long, ? extends SBMLDocument> eldest) {
+            return (size() > maxSize)
+        }
+    }
+
+    /**
+     * The internal cache.
+     */
+    private InternalCache<Long, SBMLDocument> cache
+    /**
+     * Lock to protect access to the cache.
+     */
+    private static final ReentrantLock lock = new ReentrantLock()
+
+    /**
+     * Constructor for the Cache taking the maximum cache size as argument.
+     * @param maxSize
+     */
+    public SbmlCache(int maxSize) {
+        this.cache = new InternalCache<Long, SBMLDocument>(maxSize)
+    }
+
+    int size() {
+        int size = 0
+        lock.lock()
+        try {
+            size = cache.size()
+        } finally {
+            lock.unlock()
+        }
+        return size
+    }
+
+    boolean isEmpty() {
+        boolean empty = false
+        lock.lock()
+        try {
+            empty = cache.isEmpty()
+        } finally {
+            lock.unlock()
+        }
+        return empty
+    }
+
+    boolean containsKey(Object key) {
+        if (key instanceof RevisionTransportCommand) {
+            boolean contains = false
+            lock.lock()
+            try {
+                contains = cache.containsKey(key.id)
+            } finally {
+                lock.unlock()
+            }
+            return contains
+        } else {
+            return false
+        }
+    }
+
+    boolean containsValue(Object value) {
+        if (value instanceof SBMLDocument) {
+            boolean contains = false
+            lock.lock()
+            try {
+                contains = cache.containsValue(value)
+            } finally {
+                lock.unlock()
+            }
+            return contains
+        } else {
+            return false
+        }
+    }
+
+    V get(Object key) {
+        if (key instanceof RevisionTransportCommand) {
+            V value = null
+            lock.lock()
+            try {
+                value = (V)cache.get(key.id)
+            } finally {
+                lock.unlock()
+            }
+        } else {
+            return null
+        }
+    }
+
+    V put(K key, V value) {
+        V retValue = null
+        lock.lock()
+        try {
+            retValue = (V)cache.put(key.id, value)
+        } finally {
+            lock.unlock()
+        }
+        return retValue
+    }
+
+    V remove(Object key) {
+        if (key instanceof RevisionTransportCommand) {
+            V value = null
+            lock.lock()
+            try {
+                value = (V)cache.remove(key.id)
+            } finally {
+                lock.unlock()
+            }
+            return value
+        } else {
+            return null;
+        }
+    }
+
+    void putAll(Map<? extends K, ? extends V> m) {
+        Map<Long, V> entries = [:]
+        m.each{ k, v ->
+            entries.put(k.id, v)
+        }
+        lock.lock()
+        try {
+            cache.putAll(entries)
+        } finally {
+            lock.unlock()
+        }
+    }
+
+    void clear() {
+        lock.lock()
+        try {
+            cache.clear()
+        } finally {
+            lock.unlock()
+        }
+    }
+
+    Set<K> keySet() {
+        Set<? extends Long> keys = []
+        lock.lock()
+        try {
+            keys = cache.keySet()
+        } finally {
+            lock.unlock()
+        }
+        Set<K> revisions = []
+        keys.each {
+            revisions.add((K)(new RevisionTransportCommand(id: it)))
+        }
+        return revisions
+    }
+
+    Collection<SBMLDocument> values() {
+        Collection<SBMLDocument> retVals = []
+        lock.lock()
+        try {
+            retVals = cache.values()
+        } finally {
+            lock.unlock()
+        }
+        return retVals
+    }
+
+    Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> entries = []
+        lock.lock()
+        try {
+            cache.entrySet().each {
+                entries.add(new SimpleEntry<RevisionTransportCommand, SBMLDocument>(new RevisionTransportCommand(id: it.key), it.value))
+            }
+        } finally {
+            lock.unlock()
+        }
+        return entries
+    }
+}
---
+++ jummp-plugins/jummp-plugin-sbml/test/unit/net/biomodels/jummp/plugins/sbml/SbmlCacheTest.groovy
@@ -0,0 +1,129 @@
+package net.biomodels.jummp.plugins.sbml
+
+import grails.test.GrailsUnitTestCase
+import net.biomodels.jummp.core.model.RevisionTransportCommand
+import org.sbml.jsbml.SBMLDocument
+import java.util.Map.Entry
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: graessli
+ * Date: May 2, 2011
+ * Time: 11:48:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class SbmlCacheTest extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testMaxSize() {
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(1)
+        assertTrue(cache.isEmpty())
+        assertEquals(0, cache.size())
+        cache.put(new RevisionTransportCommand(id: 1), new SBMLDocument())
+        assertEquals(1, cache.size())
+        SBMLDocument doc = new SBMLDocument(3, 1)
+        cache.put(new RevisionTransportCommand(id: 2), doc)
+        assertEquals(1, cache.size())
+        assertArrayEquals([doc].toArray(), cache.values().toArray())
+        // test for cache size of ten
+        cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(10)
+        assertTrue(cache.isEmpty())
+        (1..10).each { i ->
+            cache.put(new RevisionTransportCommand(id: i), new SBMLDocument())
+            assertEquals(i, cache.size())
+        }
+        assertEquals(10, cache.size())
+        cache.put(new RevisionTransportCommand(id: 100), new SBMLDocument())
+        assertEquals(10, cache.size())
+    }
+
+    void testPut() {
+        // tests that put works correctly
+        SBMLDocument doc = new SBMLDocument()
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(1)
+        assertNull(cache.put(new RevisionTransportCommand(id: 1), doc))
+        assertSame(doc, cache.put(new RevisionTransportCommand(id: 1), new SBMLDocument()))
+        assertNull(cache.put(new RevisionTransportCommand(id: 2), new SBMLDocument()))
+        assertNotNull(cache.put(new RevisionTransportCommand(id: 2), new SBMLDocument()))
+    }
+
+    void testPutAll() {
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(5)
+        Map<RevisionTransportCommand, SBMLDocument> entries = [:]
+        (1..5).each { i ->
+            entries.put(new RevisionTransportCommand(id: i), new SBMLDocument())
+        }
+        assertTrue(cache.isEmpty())
+        cache.putAll entries
+        assertEquals(5, cache.size())
+        (1..6).each { i ->
+            entries.put(new RevisionTransportCommand(id: i), new SBMLDocument())
+        }
+        cache.putAll entries
+        assertEquals(5, cache.size())
+    }
+
+    void testContains() {
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(5)
+        SBMLDocument doc = new SBMLDocument()
+        assertFalse(cache.containsKey(new RevisionTransportCommand(id: 1)))
+        assertFalse(cache.containsValue(doc))
+        RevisionTransportCommand revision = new RevisionTransportCommand(id: 1)
+        cache.put(revision, doc)
+        assertTrue(cache.containsKey(revision))
+        assertTrue(cache.containsKey(new RevisionTransportCommand(id: 1)))
+        assertTrue(cache.containsValue(doc))
+        cache.put(new RevisionTransportCommand(id: 1), new SBMLDocument(3, 1))
+        assertTrue(cache.containsKey(revision))
+        assertFalse(cache.containsValue(doc))
+        cache.put(new RevisionTransportCommand(id: 2), doc)
+        assertTrue(cache.containsKey(revision))
+        assertTrue(cache.containsKey(new RevisionTransportCommand(id: 2)))
+        assertTrue(cache.containsValue(doc))
+    }
+
+    void testRemove() {
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(1)
+        SBMLDocument doc = new SBMLDocument()
+        assertNull(cache.remove(new RevisionTransportCommand(id: 1)))
+        cache.put(new RevisionTransportCommand(id: 1), doc)
+        assertSame(doc, cache.remove(new RevisionTransportCommand(id: 1)))
+        assertNull(cache.remove(new RevisionTransportCommand(id: 1)))
+        cache.put(new RevisionTransportCommand(id: 1), doc)
+        cache.put(new RevisionTransportCommand(id: 2), doc)
+        assertNull(cache.remove(new RevisionTransportCommand(id: 1)))
+        assertFalse(cache.isEmpty())
+    }
+
+    void testEntries() {
+        SbmlCache<RevisionTransportCommand, SBMLDocument> cache = new SbmlCache<RevisionTransportCommand, SBMLDocument>(5)
+        SBMLDocument doc = new SBMLDocument()
+        SBMLDocument doc1 = new SBMLDocument(3, 1)
+        SBMLDocument doc2 = new SBMLDocument(2, 1)
+        SBMLDocument doc3 = new SBMLDocument(2, 2)
+        SBMLDocument doc4 = new SBMLDocument(2, 3)
+        SBMLDocument doc5 = new SBMLDocument(2, 4)
+        cache.put(new RevisionTransportCommand(id: 1), doc)
+        cache.put(new RevisionTransportCommand(id: 2), doc1)
+        cache.put(new RevisionTransportCommand(id: 3), doc2)
+        cache.put(new RevisionTransportCommand(id: 4), doc3)
+        cache.put(new RevisionTransportCommand(id: 5), doc4)
+        cache.put(new RevisionTransportCommand(id: 6), doc5)
+        assertArrayEquals([doc1, doc2, doc3, doc4, doc5].toArray(), cache.values().toArray())
+        assertArrayEquals([2, 3, 4, 5, 6].toArray(), cache.keySet().toList().sort { it.id }.collect { it.id }.toArray())
+        List<Entry<RevisionTransportCommand, SBMLDocument>> entries = cache.entrySet().toList().sort { it.key.id }
+        assertArrayEquals([2, 3, 4, 5, 6].toArray(), entries.collect { it.key.id }.toArray())
+        assertArrayEquals([doc1, doc2, doc3, doc4, doc5].toArray(), entries.collect { it.value }.toArray())
+        assertSame(doc1, cache.get(new RevisionTransportCommand(id: 2)))
+        assertSame(doc2, cache.get(new RevisionTransportCommand(id: 3)))
+        assertSame(doc3, cache.get(new RevisionTransportCommand(id: 4)))
+        assertSame(doc4, cache.get(new RevisionTransportCommand(id: 5)))
+        assertSame(doc5, cache.get(new RevisionTransportCommand(id: 6)))
+    }
+}
