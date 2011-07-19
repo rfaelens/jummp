a887cf9a624a06bc2ab0ae7a96e6494859e4bbaa
Martin Gräßlin <m.graesslin@dkfz.de>
2011-07-19 14:29:58 +0200

    Custom queries for retrieving the Revisions for a Gene Ontology
    Restrictions on not deleted revision and on the revisions the user has access to.
    And only the revision with the highest revision number per Model.

--- grails-app/services/net/biomodels/jummp/core/GeneOntologyTreeService.groovy
+++ grails-app/services/net/biomodels/jummp/core/GeneOntologyTreeService.groovy
@@ -4,6 +4,11 @@ import net.biomodels.jummp.core.miriam.GeneOntology
 import net.biomodels.jummp.core.miriam.GeneOntologyRelationship
 import net.biomodels.jummp.core.miriam.GeneOntologyTreeLevel
 import net.biomodels.jummp.core.miriam.GeneOntologyRelationshipType
+import org.springframework.security.acls.domain.BasePermission
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import net.biomodels.jummp.model.Revision
+import org.codehaus.groovy.grails.plugins.springsecurity.acl.AclObjectIdentity
+import org.springframework.security.core.userdetails.UserDetails
 
 /**
  * @short Service to retrieve Gene Ontology Tree information.
@@ -11,6 +16,14 @@ import net.biomodels.jummp.core.miriam.GeneOntologyRelationshipType
  * @author Martin Gräßlin <m.graesslin@dkfz.de>
  */
 class GeneOntologyTreeService {
+    /**
+     * Dependency Injection of Spring Security Service
+     */
+    def springSecurityService
+    /**
+     * Dependency Injection of AclUtilService
+     */
+    def aclUtilService
 
     static transactional = true
 
@@ -46,7 +59,7 @@ class GeneOntologyTreeService {
             level.addOntology(go.id, go.description.identifier, go.description.name, type)
         }
         if (geneOntology) {
-            geneOntology.revisions.each {
+            revisionsForGeneOntology(geneOntology).each {
                 level.addRevision(it.toCommandObject())
             }
         }
@@ -78,5 +91,59 @@ class GeneOntologyTreeService {
             return []
         }
         return GeneOntology.executeQuery("SELECT DISTINCT rel.from, rel.type FROM GeneOntologyRelationship rel WHERE rel.to=:go", [go: go])
+    }
+
+    private List<Revision> revisionsForGeneOntology(GeneOntology go) {
+        // First retrieve all IDs of the Revision which are not deleted
+        List<Long> ids = Revision.executeQuery("SELECT rev.id FROM GeneOntology AS go LEFT JOIN go.revisions AS rev WHERE rev.deleted = false AND go=:go", [go: go]) as List<Long>
+        // second: restrict on the revisions the current user can see
+        if (!ids.isEmpty() && SpringSecurityUtils.ifNotGranted("ROLE_ADMIN")) {
+            Set<String> roles = SpringSecurityUtils.authoritiesToRoles(SpringSecurityUtils.getPrincipalAuthorities())
+            if (springSecurityService.isLoggedIn()) {
+                // anonymous users do not have a principal
+                roles.add((springSecurityService.getPrincipal() as UserDetails).getUsername())
+            }
+            // checks whether the User has access to the given revisions
+            // a user has access to it if a ACE is defined with read or administration right
+            // the query assumes that there are no inheriting and no non-granting ACEs
+            ids = AclObjectIdentity.executeQuery('''
+                    SELECT DISTINCT aoi.objectId FROM AclEntry AS ace
+                    INNER JOIN ace.aclObjectIdentity AS aoi
+                    INNER JOIN ace.sid AS sid
+                    INNER JOIN aoi.aclClass AS c
+                    WHERE
+                    c.className=:className
+                    AND
+                    aoi.objectId IN (:objectIds)
+                    AND
+                    ace.mask IN (:permissions)
+                    AND
+                    ace.granting=true
+                    AND
+                    sid.sid IN (:roles)
+                    ''', [
+                    className: Revision.class.getName(),
+                    objectIds: ids,
+                    permissions: [BasePermission.READ.getMask(), BasePermission.ADMINISTRATION.getMask()],
+                    roles: roles]) as List
+        }
+        if (ids.isEmpty()) {
+            return []
+        } else if (ids.size() == 1) {
+            return [Revision.get(ids[0])]
+        } else {
+            // third: only one revision per Model
+            // restricts on the maximum revisionNumber per Model
+            // and on the Ids present in the list
+            return Revision.executeQuery('''
+                SELECT rev
+                FROM Revision AS rev
+                JOIN rev.model.revisions AS revisions
+                WHERE
+                rev.id IN (:ids)
+                GROUP BY rev.model, rev.id
+                HAVING rev.revisionNumber = max(revisions.revisionNumber)
+                ORDER BY rev.model''', [ids: ids])
+        }
     }
 }
