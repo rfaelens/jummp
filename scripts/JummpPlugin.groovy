258f60fcbd22f1551dfc45cecefd7ef1fd5d6908
Martin Gräßlin <m.graesslin@dkfz.de>
2011-09-16 15:03:11 +0200

    Remove last plugin jar after successful build of new jar

--- scripts/JummpPlugin.groovy
+++ scripts/JummpPlugin.groovy
@@ -5,5 +5,10 @@ target(jummpPlugin: "Compiles a JUMMP Plugin and moves it to the shared Plugin D
     ant.property(file: "build.number")
     String jarName = "grails-plugin-${grailsSettings.baseDir.name}-0.1"
     depends(packagePlugin)
+    Integer buildNumber = Integer.parseInt(ant.project.properties.'build.number')
+    if (buildNumber != 0) {
+        buildNumber--
+        ant.delete(file: "../../pluginlibs/${jarName}-${buildNumber}.jar")
+    }
     ant.move(file: "target/${jarName}.jar", toFile: "../../pluginlibs/${jarName}-${ant.project.properties.'build.number'}.jar")
 }
