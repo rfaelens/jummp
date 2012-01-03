a8522178cefe3248bfc644b3566030bdfc032f9e
Martin Gräßlin <m.graesslin@dkfz.de>
2012-01-03 13:57:21 +0100

    Fix admin creation in setup flow
    That was broken for several months without anybody noticing...
    This has to become better.

--- jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/UserCommand.groovy
+++ jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/UserCommand.groovy
@@ -8,12 +8,16 @@ class UserCommand implements Serializable {
     private static final long serialVersionUID = 1L
     String username
     String password
+    String rePassword
     String userRealName
     String email
 
     static constraints = {
         username( blank: false)
         password( blank: false)
+        rePassword( validator: { val, obj ->
+            return obj.password == val
+        })
         userRealName(blank: false)
         email(email: true)
     }
