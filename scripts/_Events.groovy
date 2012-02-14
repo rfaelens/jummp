e5e4d191f8f241c8ed06bbe82ed378e7c00041a3
Martin Gräßlin <m.graesslin@dkfz.de>
2012-02-14 10:31:18 +0100

    Include build number in default footer

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -9,6 +9,19 @@ eventSetClasspath = { rootLoader ->
     ast()
 }
 
+/**
+ * Script to write the mercurial id of the build into _version.gsp
+ *
+ * Based upon an example from
+ * http://blog.armbruster-it.de/2011/07/embedding-the-githgsvns-revision-number-inside-a-grails-application/
+ */
+eventCompileStart = { msg ->
+    def proc = "hg id -i".execute()
+    proc.waitFor()
+    ant.mkdir(dir: "grails-app/views/templates/")
+    new FileOutputStream("grails-app/views/templates/_version.gsp", false) << proc.in.text
+}
+
 eventCompileEnd = {
     weceem()
 }
