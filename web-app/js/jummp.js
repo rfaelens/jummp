cb0ce68e6b64cbdc9489f93a17012b58b748c59b
Jochen Schramm <j.schramm@dkfz.de>
2012-03-06 16:39:21 +0100

    Strip down to what is actually really needed

--- web-app/js/jummp.js
+++ web-app/js/jummp.js
@@ -1,68 +1,33 @@
-// TODO: create proper links
-$.i18n.properties({
-    name: 'messages',
-    path: '/jummp/js/i18n/',
-    mode: "map"
-});
-
 $.jummp = new Object();
 
 $.jummp.message = function(message, warning) {
     if (!message) {
         return;
     }
-    var classes = "message";
-    if (warning) {
-        classes += " warning";
-    }
-    var button = $("<div><button></button></div>");
-    $("button", button).click(function() {
-        $(this).parent().parent().remove();
-    });
-    var divCode = $("<div class=\"" + classes + "\"><p>" + message + "</p></div>");
-    button.appendTo(divCode);
-    $("#infoBox").append(divCode);
-};
-
-$.jummp.infoMessage = function(message) {
-    this.message(message, false);
-};
-
-$.jummp.warningMessage = function(message) {
-    this.message(message, true);
-};
+}
 
 /**
- * Loads a new view for the #body element through AJAX.
- * @param url The URL from where to load the view
- * @param loadCallback A callback to execute after successfully updating the view.
- * @param callbackData Additional data to be passed to the callback
+ * Same as g.createLink.
+ * @param controller The name of the grails controller
+ * @param action The optional action
+ * @param id The optional id
  */
-function loadView(url, loadCallback, callbackData) {
-    //$('div').filter('#main').block();
-    //alert(jQuery('div').filter('#main').length + ' main divs');
-    alert('url: ' + url + 'loadCallback: ' + loadCallback + ' callbackData: ' + callbackData)
-    //$("#body").block();
-    $.ajax({
-        url: url,
-        dataType: 'HTML',
-        type: 'GET',
-        cache: 'false',
-        success: function(data) {
-            alert('here!')
-            $("#body").unblock();
-            clearErrorMessages();
-            $("#body").html(data);
-            loadCallback(data, callbackData);
-        },
-        error: function(jqXHR) {
-            $("#body").unblock();
-        },
-        statusCode: {
-            400: handler400,
-            403: handler403,
-            404: handler404,
-            500: handler500
+function createLink(controller, action, id) {
+    var path = controller;
+    if (action != undefined) {
+        path += "/" + action;
+        if (id != undefined) {
+            path += "/" + id;
         }
-    });
+    }
+    return createURI(path);
+}
+
+/**
+ * Creates a URI to be used in a href or src HTML attribute.
+ * @param path The path
+ */
+function createURI(path) {
+   // console.log("$.appName: " + $.appName)
+    return "/" + $.appName + "/" + path;
 }
