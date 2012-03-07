f323765b406c02db7c646237928b5f527db8077b
Martin Gräßlin <m.graesslin@dkfz.de>
2012-03-07 07:58:57 +0100

    Revert removed code from 7228ebeeb7d7

--- web-app/js/jummp.js
+++ web-app/js/jummp.js
@@ -1,10 +1,36 @@
+// TODO: create proper links
+$.i18n.properties({
+    name: 'messages',
+    path: '/jummp/js/i18n/',
+    mode: "map"
+});
+
 $.jummp = new Object();
 
 $.jummp.message = function(message, warning) {
     if (!message) {
         return;
     }
-}
+    var classes = "message";
+    if (warning) {
+        classes += " warning";
+    }
+    var button = $("<div><button></button></div>");
+    $("button", button).click(function() {
+        $(this).parent().parent().remove();
+    });
+    var divCode = $("<div class=\"" + classes + "\"><p>" + message + "</p></div>");
+    button.appendTo(divCode);
+    $("#infoBox").append(divCode);
+};
+
+$.jummp.infoMessage = function(message) {
+    this.message(message, false);
+};
+
+$.jummp.warningMessage = function(message) {
+    this.message(message, true);
+};
 
 /**
  * Same as g.createLink.
