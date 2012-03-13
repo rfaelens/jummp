8bd6abdd578b5c682ae0f38cd602654eeaa21c37
Jochen Schramm <j.schramm@dkfz.de>
2012-03-13 15:46:43 +0100

    Initial commit of SBML tag lib
    By now it only renders the notes.

---
+++ jummp-plugins/jummp-plugin-web-application/grails-app/taglib/net/biomodels/jummp/webapp/SbmlTagLib.groovy
@@ -0,0 +1,57 @@
+package net.biomodels.jummp.webapp
+
+class SbmlTagLib {
+
+    static namespace = "sbml"
+
+    /**
+     * Renders an SBML notes string.
+     * @attr notes REQUIRED The notes string
+     */
+    def notes = { attrs ->
+        if (!attrs.notes || attrs.notes == "") {
+            return
+        }
+        // the notesString returned by the core is wrapped in a <notes><body></body></notes>
+        // we parse the String and build a new XML String from the GPath representation
+        // at the same time we also drop the dangerous script tag
+        // for reconstructing the HTML we use a closure
+        def rootNode = new XmlSlurper(false, false).parseText(attrs.notes)
+        String html = ""
+        def closure = { element, closure ->
+            if (!(element instanceof groovy.util.slurpersupport.Node)) {
+                return element
+            }
+            String text = ""
+            if (element.name.toLowerCase() == "script") {
+                return ""
+            }
+            text += "<${element.name}"
+            element.attributes.each { name, value ->
+                // TODO: the attributes may contain JavaScript, this should be stripped
+                text += " ${name}=\"${value}\""
+            }
+            if (element.children.isEmpty()) {
+                text += "/>"
+                return text
+            }
+            text += ">"
+            element.children.each {
+                text += closure(it, closure)
+            }
+            text += "</${element.name}>"
+            return text
+        }
+        rootNode.body.childNodes().each { child ->
+            html += closure(child, closure)
+        }
+        // not all notes are wrapped in a body
+        rootNode.childNodes().each { child ->
+            // body already handled above
+            if (child.name.toLowerCase() != "body") {
+                html += closure(child, closure)
+            }
+        }
+        out << html
+    }
+}
