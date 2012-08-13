e03f4aa14495eee5de9c4b2504b91f5ccc340fb0
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2012-08-13 15:33:47 +0200

    Adjust SbmlServiceTests to changes in latest SBML version
    Since the update of JSBML with 8f99b448c9901dd4555ad955f9227dd02ca1f07f
    the modelNotes are returned in a different format.

--- test/integration/net/biomodels/jummp/plugins/SbmlServiceTests.groovy
+++ test/integration/net/biomodels/jummp/plugins/SbmlServiceTests.groovy
@@ -93,7 +93,7 @@ class SbmlServiceTests extends JummpIntegrationTest {
   </model>
 </sbml>''')
         RevisionTransportCommand rev2 = modelService.addRevision(model, modelWithNotes, ModelFormat.findByIdentifier("SBML"), "test").toCommandObject()
-        assertEquals('''<notes><body xmlns="http://www.w3.org/1999/xhtml"><p>Test</p></body></notes>''', sbmlService.getNotes(rev2))
+        assertEquals('''<notes>\n  <body xmlns="http://www.w3.org/1999/xhtml">\n<p>Test</p>\n    </body>\n  \n</notes>''', sbmlService.getNotes(rev2))
     }
 
     private void setupVcs() {
