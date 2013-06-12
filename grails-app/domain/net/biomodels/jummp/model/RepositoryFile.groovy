56264dae60a34048421773fa523d10c2ed833d25
Mihai Glonț <mglont@ebi.ac.uk>
2013-06-12 19:28:52 +0100

    Introduced validation for RepositoryFile.Added automatic mime type detection.
    The content type discovery relies on Apache Tika, hence the relevant component
    has been added as a dependency. RepositoryFile now has an extra boolean flag
    indicating whether this is the main submission entry or not, and a string that
    holts the content type of the file in question.

--- grails-app/domain/net/biomodels/jummp/model/RepositoryFile.groovy
+++ grails-app/domain/net/biomodels/jummp/model/RepositoryFile.groovy
@@ -1,11 +1,15 @@
 package net.biomodels.jummp.model
 
+import org.apache.tika.detect.DefaultDetector
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.mime.MediaType
+
 /**
  * @short Representation of a File belonging to a ModelVersion.
  *
  * @see net.biomodels.jummp.model.Revision
  * @author Mihai Glonț <mihai.glont@ebi.ac.uk>
- * @date 20130522
+ * @date 20130612
  */
 class RepositoryFile {
     static belongsTo = [revision:Revision]
@@ -22,15 +26,47 @@ class RepositoryFile {
      * By default, they are the former.
      */
     boolean hidden = false
+    /**
+     * Flag for distinguishing the main entry of a submission from the rest. This will be considered the main
+     * model file. For instance, in the case of DDMoRe submissions this should be the PharmML file. If set to true,
+     * this must be in XML or ZIP format.
+     */
+    boolean mainFile = false
+    /**
+     * Set to true if this file was provided by the user during submission, rather than automatically generated.
+     */
+    boolean userSubmitted = false
+    /**
+     * The content type of this file as defined in http://www.iana.org/assignments/media-types/
+     * 
+     */
+    String mimeType
 
     static constraints = {
-        path(nullable: false, blank: false, unique: true,
-            validator: { p -> 
+        path(blank: false, unique: true,
+            validator: { p, rf -> 
                 String sep  = File.separator
                 String pathRegex = "${sep}?([a-zA-Z0-9\\-_]+${sep})+[a-zA-Z0-9\\-_\\.]+".toString()
-                return p != null && new File(p).exists() && p.matches(pathRegex)
+                if (p == null || !new File(p).exists() || ! p.matches(pathRegex)) {
+                    return false
+                } 
+                def f = new File(p).getCanonicalFile()
+                def sherlock = new DefaultDetector()
+                String properType = sherlock.detect(new BufferedInputStream(
+                        new FileInputStream(f)), new Metadata()).toString()
+                if (!rf.mimeType.equals(properType)) {
+                    rf.mimeType = properType
+                    return true
+                }
+            })
+        description(blank: true, maxSize:500)
+        //content type detection is performed above, when we validate the path of the file
+        mimeType(nullable: true, blank: true)
+        mainFile(validator: { main, rf ->
+            if (main) {
+                return !rf.hidden
             }
-        )
-        description(nullable: false, blank: true, maxSize:100)
+            return true
+        })
     }
 }
--- test/unit/net/biomodels/jummp/model/RepositoryFileTests.groovy
+++ test/unit/net/biomodels/jummp/model/RepositoryFileTests.groovy
@@ -10,18 +10,7 @@ class RepositoryFileTests {
     @Before
     public void setUp() {
         new File("target/vcs/aaa/model1/").mkdirs()
-        def test = new File("target/vcs/aaa/model1/test.txt")
-        def fout
-        try {
-            fout = new BufferedWriter(new FileWriter(test))
-            fout.append("This is a test\n")
-        } catch (IOException ignored) {
-        }
-        finally {
-            fout.flush()
-            fout.close()
-        }
-    }
+     }
 
     @After
     public void tearDown() {
@@ -32,7 +21,7 @@ class RepositoryFileTests {
     void testConstraints() {
         def f = createFile("target/vcs/aaa/model1/m1.xml")
         def repositoryFile = new RepositoryFile(path: f.absolutePath, description: "Model1",
-                hidden: false, revision: null)
+               mainFile: true, hidden: false, mimeType: "application/xml", revision: null)
         mockForConstraintsTests(RepositoryFile, [repositoryFile])
 
         //nulls are not accepted
@@ -44,44 +33,62 @@ class RepositoryFileTests {
         assertEquals("nullable", repositoryFile.errors["revision"])
 
         // path cannot be blank
-        def rev = new Revision(vcsId: "1", revisionNumber: 1, minorRevision: false, uploadDate: new Date(),
-                comment: "", owner: null, model: null)
-        repositoryFile = new RepositoryFile(path: "", description: "nothing", hidden: false, revision: rev)
+        def rev = new Revision(vcsId: "1", revisionNumber: 1, minorRevision: false,
+                uploadDate: new Date(), comment: "", owner: null, model: null)
+        repositoryFile = new RepositoryFile(path: "", description: "nothing", hidden: false,
+                mainFile: false, mimeType: "application/octet-stream", revision: rev)
         assertFalse(repositoryFile.validate())
         assertEquals("blank", repositoryFile.errors["path"])
         assertEquals(1, repositoryFile.errors.getErrorCount())
 
-        // empty file descriptions are acceptable
+        // empty file descriptions and mimeTypes are acceptable
         def newPath = createFile("target/vcs/aaa/model1/m1a.xml").absolutePath
         assertTrue(new File(newPath).exists())
-        def repositoryFile2 = new RepositoryFile(path: newPath, description: "", hidden: false, revision: new Revision())
+        def repositoryFile2 = new RepositoryFile(path: newPath, description: "", mainFile: false,
+                mimeType: "", hidden: false, revision: new Revision())
         assertTrue(repositoryFile2.validate())
-        assertNull(repositoryFile2.errors["description"])
 
         //paths must be unique
         mockForConstraintsTests(RepositoryFile, [repositoryFile2])
         def duplicateFile = new RepositoryFile(path: newPath, description: "",
-            hidden: false, revision: rev)
+            mainFile: true, hidden: false, mimeType: "text/plain", revision: rev)
         assertFalse(duplicateFile.validate())
         assertEquals(1, duplicateFile.errors.getErrorCount())
         assertEquals("unique", duplicateFile.errors["path"])
 
-        //descriptions can have at-most 100 characters
-        repositoryFile2.description = "a"*100
+        //descriptions can have at-most 500 characters
+        repositoryFile2.description = "a"*500
         assertTrue(repositoryFile2.validate())
         assertNull(repositoryFile2.errors["description"])
         repositoryFile2.description += "b"
         assertFalse(repositoryFile2.validate())
         assertEquals(1, repositoryFile2.errors.getErrorCount())
         assertEquals("maxSize", repositoryFile2.errors["description"])
+
+        //main submission entries cannot be hidden from the user
+        def mainFile = createFile("target/vcs/aaa/model1/m1b.xml")
+        def rf = new RepositoryFile(path: mainFile.absolutePath, description: "Silly model",
+                mainFile: true, mimeType: "", hidden: true, revision: new Revision())
+        assertFalse(rf.validate())
+        assertEquals(1, rf.errors.getErrorCount())
+        assertEquals("validator", rf.errors["mainFile"])
+        rf.mainFile = false
+        assertTrue(rf.validate())
+        rf.mainFile = true
+        rf.hidden = false
+        assertTrue(rf.validate())
     }
 
     private File createFile(String path) {
         if (path == null) {
-            path = ".${File.separator}error.txt".toString()
+            path = "error.xml"
         }
-        File f = new File(path)
-        FileUtils.touch(f)
+        def f = new File(path)
+        f.text = '''<?xml version="1.0"?>
+    <foo>
+        <bar>baz</bar>
+    </foo>
+'''
         return f.getCanonicalFile()
     }
 }
