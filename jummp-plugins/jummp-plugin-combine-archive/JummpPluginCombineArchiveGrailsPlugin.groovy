c8408a09a11e9befdb902328d0b9cfe8d264e085
Mihai Glonț <mglont@ebi.ac.uk>
2013-08-02 18:30:38 +0300

    First commit for the COMBINE archive plugin.
    Added libCombineArchive as a dependency and updated the placeholders as
    well as the load order of the plugin in the descriptor.
    
    And git stash save is not fun when you do not include the changes that
    are already in the staging area!

---
+++ jummp-plugins/jummp-plugin-combine-archive/JummpPluginCombineArchiveGrailsPlugin.groovy
@@ -0,0 +1,72 @@
+class JummpPluginCombineArchiveGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.2 > *"
+    def loadAfter = ['jummp-plugin-security', 'jummp-plugin-core-api']
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def title = "Jummp Plugin Combine Archive Plugin" // Headline display name of the plugin
+    def author = "Your name"
+    def authorEmail = ""
+    def description = '''\
+Brief summary/description of the plugin.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jummp-plugin-combine-archive"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+//    def license = "APACHE"
+
+    def organization = [
+        name: "EMBL-European Bioinformatics Institute",
+        url: "http://www.ebi.ac.uk/"
+    ]
+
+    def developers = [ 
+        [ name: "Mihai Glonț", email: "mihai.glont@ebi.ac.uk" ]
+    ]
+
+    // Location of the plugin's issue tracker.
+//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+
+    def scm = [ url: "http://bitbucket.org/jummp/jummp/" ]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
---
+++ jummp-plugins/jummp-plugin-combine-archive/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,38 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    inherits("global") {
+    }
+    log "warn"
+    // circumvent http://jira.grails.org/browse/GRAILS-9984
+    legacyResolve true
+    repositories {
+        if (System.getenv("JUMMP_ARTIFACTORY_URL")) {
+            mavenRepo "${System.getenv('JUMMP_ARTIFACTORY_URL')}"
+        }
+        if (System.getenv("JUMMP_ARTIFACTORY_GRAILS_PLUGINS_URL")) {
+            grailsRepo "${System.getenv('JUMMP_ARTIFACTORY_GRAILS_PLUGINS_URL')}"
+        }
+        grailsCentral()
+        mavenCentral()
+        mavenRepo "http://www.ebi.ac.uk/~maven/m2repo"
+        mavenRepo "http://www.ebi.ac.uk/~maven/m2repo_snapshots/"
+    }
+    dependencies {
+        compile("org.mbine.co:libCombineArchive:0.1-SNAPSHOT") {
+            excludes 'junit', 'slf4j-api', 'slf4j-log4j12'
+        }
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.2.1",
+              ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
+grails.plugin.location.'jummp-plugin-security' = "../jummp-plugin-security"
+grails.plugin.location.'jummp-plugin-core-api' = "../jummp-plugin-core-api"
