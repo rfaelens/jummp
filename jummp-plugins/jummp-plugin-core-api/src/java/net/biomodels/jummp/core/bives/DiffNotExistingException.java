cf9a84ddb3ce275c815c0dfac486149cff6baadb
Raza <raza.ali@ebi.ac.uk>
2013-10-31 17:59:02 +0000

    Adding the license and permission
    License added to every file (groovy/java/gsp.. todo: javascript).
    Also added generic permissions text to any file that had an
    import that wasnt net.biomodels. Need to go through everywhere
    where there is this text and see which license needs to be referred
    and which plugin needs to be named, if any.

--- jummp-plugins/jummp-plugin-core-api/src/java/net/biomodels/jummp/core/bives/DiffNotExistingException.java
+++ jummp-plugins/jummp-plugin-core-api/src/java/net/biomodels/jummp/core/bives/DiffNotExistingException.java
@@ -1,4 +1,17 @@
 /**
+* Copyright (C) 2010-2013 EMBL-European Bioinformatics Institute (EMBL-EBI), Deutsches Krebsforschungszentrum (DKFZ)
+*
+* This file is part of Jummp.
+*
+* Jummp is free software; you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+*
+* Jummp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License along with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+**/
+
+
+/**
  * This file is part of the project bives.jummp, and thus part of the
  * implementation for the diploma thesis "Versioning Concepts and Technologies
  * for Biochemical Simulation Models" by Robert Haelke, Copyright 2010.
