ed4d3b79e065cdb4deffb51139b126fb25b1b715
Mihai Glonț <mglont@ebi.ac.uk>
2013-11-25 17:32:28 +0000

    Revert "Running tests in forked mode too."
    This reverts commit 22679f250b711538abe292a57b71318797c23fdf.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,7 +37,7 @@ grails.project.dependency.resolver = "maven"
 customJvmArgs = ["-server", "-noverify", "-XX:+UseConcMarkSweepGC", "-XX:+UseParNewGC" ]
 grails.project.fork = [
     // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 2048, minMemory: 64, debug: false, maxPerm: 512, daemon:true],
+    test: false, //[maxMemory: 2048, minMemory: 64, debug: false, maxPerm: 512, daemon:true],
     // configure settings for the run-app JVM
     run: [maxMemory: 2048, minMemory: 64, debug: false, maxPerm: 512, forkReserve:false, jvmArgs: customJvmArgs],
     // configure settings for the run-war JVM
