1d1a544eef0de43092eea97e876a45c649b00032
Raza <raza.ali@ebi.ac.uk>
2013-11-26 15:10:08 +0000

    Changed test omex archive to one with trickier name
    As this caused a bug previously, the filename for the
    omex archive is modified to include a space. The tests have
    been updated accordingly.

--- test/integration/net/biomodels/jummp/plugins/webapp/SubmissionFlowTests.groovy
+++ test/integration/net/biomodels/jummp/plugins/webapp/SubmissionFlowTests.groovy
@@ -461,8 +461,8 @@ class SubmissionFlowTests extends JummpIntegrationTest {
 
     class TestSubmitOmex extends TestUploadFiles {
         void performRemainingTest() {
-            final File MODEL_FILE = new File("jummp-plugins/jummp-plugin-combine-archive/test/files/sample.omex")
-            fileUploadPipeline(MODEL_FILE, "OMEX", "sample", ["sample.omex"] as String[])
+            final File MODEL_FILE = new File("jummp-plugins/jummp-plugin-combine-archive/test/files/sample archive.omex")
+            fileUploadPipeline(MODEL_FILE, "OMEX", "sample archive", ["sample archive.omex"] as String[])
         }
     }
 
