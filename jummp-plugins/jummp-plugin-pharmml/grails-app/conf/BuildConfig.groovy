364e4b2c8b06bbb796e81d2a151e0f7efd70d50e
Mihai Glonț <mglont@ebi.ac.uk>
2014-06-05 16:08:24 +0100

    Upgraded to libPharmML 0.3.1-b2

--- jummp-plugins/jummp-plugin-pharmml/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-pharmml/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        compile("eu.ddmore.pharmml:libPharmML:0.3.1") {
+        compile("eu.ddmore.pharmml:libPharmML:0.3.1-b2") {
             excludes 'junit-dep'
         }
         runtime("commons-jexl:commons-jexl:1.1") { excludes 'junit', 'commons-logging' }
