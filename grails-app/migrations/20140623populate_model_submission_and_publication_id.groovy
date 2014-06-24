5323fb61b939f5b3b4949e414284ada39d35754c
Raza <raza.ali@ebi.ac.uk>
2014-06-24 15:21:26 +0100

    Fix to submission_id migration by Mihai

--- grails-app/migrations/20140623populate_model_submission_and_publication_id.groovy
+++ grails-app/migrations/20140623populate_model_submission_and_publication_id.groovy
@@ -1,14 +1,18 @@
 import net.biomodels.jummp.core.model.identifier.generator.NullModelIdentifierGenerator
+import net.biomodels.jummp.core.model.identifier.decorator.AbstractAppendingDecorator
 
 databaseChangeLog = {
-    changeSet(author: "Mihai Glont", id: "populate-submission-id-20140622") {
-        grailsChange {
-            change {
-                def sig = ctx.getBean("submissionIdGenerator")
-                sql.eachRow("select * from model") { m ->
-                    m.submission_id = sig.generate()
-                }
-            }
-        }
-    }
+		changeSet(author: "Mihai Glont", id: "populate-submission-id-20140624") {
+				grailsChange {
+						change {
+								AbstractAppendingDecorator.context = ctx
+										def sig = ctx.getBean("submissionIdGenerator")
+										def modelTable = sql.dataSet("model")
+										modelTable.rows().each { m ->
+												String sId = sig.generate()
+												sql.executeUpdate "update model set submission_id = $sId where id = ${m.id}"
+										}
+						}
+				}
+		}
 }
