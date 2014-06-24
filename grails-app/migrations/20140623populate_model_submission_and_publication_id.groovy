6715f238e0ddaec2d299a0123ee147bf7d7002ee
Mihai Glonț <mglont@ebi.ac.uk>
2014-06-24 15:23:00 +0100

    formatting.

--- grails-app/migrations/20140623populate_model_submission_and_publication_id.groovy
+++ grails-app/migrations/20140623populate_model_submission_and_publication_id.groovy
@@ -2,17 +2,17 @@ import net.biomodels.jummp.core.model.identifier.generator.NullModelIdentifierGe
 import net.biomodels.jummp.core.model.identifier.decorator.AbstractAppendingDecorator
 
 databaseChangeLog = {
-		changeSet(author: "Mihai Glont", id: "populate-submission-id-20140624") {
-				grailsChange {
-						change {
-								AbstractAppendingDecorator.context = ctx
-										def sig = ctx.getBean("submissionIdGenerator")
-										def modelTable = sql.dataSet("model")
-										modelTable.rows().each { m ->
-												String sId = sig.generate()
-												sql.executeUpdate "update model set submission_id = $sId where id = ${m.id}"
-										}
-						}
-				}
-		}
+    changeSet(author: "Mihai Glont", id: "populate-submission-id-20140624") {
+        grailsChange {
+            change {
+                AbstractAppendingDecorator.context = ctx
+                def sig = ctx.getBean("submissionIdGenerator")
+                def modelTable = sql.dataSet("model")
+                modelTable.rows().each { m ->
+                    String sId = sig.generate()
+                    sql.executeUpdate "update model set submission_id = $sId where id = ${m.id}"
+                }
+            }
+        }
+    }
 }
