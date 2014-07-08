8a59ab3905eda117b7651af9bf7ebea885cdca4a
Mihai Glonț <mglont@ebi.ac.uk>
2014-07-08 10:16:49 +0100

    copyright and formatting.

--- jummp-plugins/jummp-plugin-web-application/src/groovy/net/biomodels/jummp/webapp/rest/search/Results.groovy
+++ jummp-plugins/jummp-plugin-web-application/src/groovy/net/biomodels/jummp/webapp/rest/search/Results.groovy
@@ -1,11 +1,30 @@
+/**
+ * Copyright (C) 2010-2014 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Deutsches Krebsforschungszentrum (DKFZ)
+ *
+ * This file is part of Jummp.
+ *
+ * Jummp is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Jummp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+ **/
+
 package net.biomodels.jummp.webapp.rest.search
 
-class Results
-{
+class Results {
     RequestParameters queryParameters
     List<ModelSummary> models = []
-                                public Results(def results)
-    {
+
+    public Results(def results) {
         results.models.each {
             models.add(new net.biomodels.jummp.webapp.rest.search.ModelSummary(it))
         }
