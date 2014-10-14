828c91f050899111b4dab0dc3dfe5ba5a2770a7e
Mihai Glonț <mglont@ebi.ac.uk>
2014-10-14 10:05:16 +0100

    JUM-17 Upgraded core-api plugin to Grails 2.3.6.

--- jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
@@ -75,9 +75,9 @@ grails.project.dependency.resolution = {
         }
     }
     plugins {
-        build ":tomcat:7.0.50"
+        build ":tomcat:7.0.50.1"
         compile ":perf4j:0.1.1"
-        runtime ":hibernate:3.6.10.7"
+        runtime ":hibernate:3.6.10.8"
     }
 }
 grails.plugin.location.'jummp-plugin-security'="../jummp-plugin-security"
