6e078cd4db4aa3ace85f278b4096dc12d41c0537
Raza <raza.ali@ebi.ac.uk>
2014-10-15 18:08:42 +0100

    Better name for notification user field

--- jummp-plugins/jummp-plugin-web-application/grails-app/domain/net/biomodels/jummp/webapp/NotificationUser.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/domain/net/biomodels/jummp/webapp/NotificationUser.groovy
@@ -35,12 +35,12 @@ class NotificationUser implements Serializable {
     Notification notification;
     User user;
     boolean mailSent = false;
-    boolean webNotificationSent = false;
+    boolean notificationSeen = false;
     
     static constraints = {
         notification(nullable: false, unique: false)
         user(nullable: false, unique: false)
         mailSent(nullable: false, unique: false)
-        webNotificationSent(nullable: false, unique: false)
+        notificationSeen(nullable: false, unique: false)
     }
 }
