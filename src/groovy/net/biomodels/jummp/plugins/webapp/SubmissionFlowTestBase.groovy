6aa66282c0240bbb6752e6d6f96f745ca4dc4f2e
Raza <raza.ali@ebi.ac.uk>
2015-01-27 18:23:28 +0000

    Refactor SubmissionFlowTests to avoid repeating code.
    Tests pass. Hope Jenkins complies.

--- src/groovy/net/biomodels/jummp/plugins/webapp/SubmissionFlowTestBase.groovy
+++ src/groovy/net/biomodels/jummp/plugins/webapp/SubmissionFlowTestBase.groovy
@@ -21,77 +21,134 @@
 
 
 package net.biomodels.jummp.plugins.webapp
-import net.biomodels.jummp.core.JummpIntegrationTest
-import static org.junit.Assert.*
 import grails.test.*
-import net.biomodels.jummp.core.JummpIntegrationTest
+import grails.test.WebFlowTestCase
+import grails.util.Holders
 import net.biomodels.jummp.core.FileSystemService
 import net.biomodels.jummp.core.model.ModelFormatTransportCommand
 import net.biomodels.jummp.core.model.ModelTransportCommand
 import net.biomodels.jummp.core.model.RepositoryFileTransportCommand
 import net.biomodels.jummp.core.model.RevisionTransportCommand as RTC
-import net.biomodels.jummp.model.Revision
-import net.biomodels.jummp.plugins.webapp.*
 import net.biomodels.jummp.model.Model
+import net.biomodels.jummp.model.Revision
+import net.biomodels.jummp.plugins.security.Person
+import net.biomodels.jummp.plugins.security.Role
+import net.biomodels.jummp.plugins.security.User
+import net.biomodels.jummp.plugins.security.UserRole
+import net.biomodels.jummp.plugins.webapp.SubmissionFlowTestBase
+import net.biomodels.jummp.webapp.ModelController
 import org.apache.commons.io.FileUtils
+import org.codehaus.groovy.grails.commons.spring.GrailsWebApplicationContext
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import org.codehaus.groovy.grails.plugins.springsecurity.acl.AclSid
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
 import org.junit.After
 import org.junit.Before
-import grails.util.Holders
 import org.junit.Test
-import net.biomodels.jummp.webapp.ModelController
+import org.springframework.binding.convert.service.DefaultConversionService
 import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.mock.web.MockMultipartFile
 import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.mock.web.MockMultipartHttpServletRequest
-import net.biomodels.jummp.webapp.ModelController
-
-import net.biomodels.jummp.plugins.security.User
-import net.biomodels.jummp.plugins.security.Person
-import org.codehaus.groovy.grails.plugins.springsecurity.acl.AclSid
-import net.biomodels.jummp.plugins.security.Role
-import net.biomodels.jummp.plugins.security.UserRole
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.mock.web.MockServletContext
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
-import org.springframework.web.servlet.ViewResolver
-
-import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.context.request.RequestAttributes
 import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.mock.web.MockMultipartHttpServletRequest
-
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistryImpl
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices
+import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator
+import static org.junit.Assert.*
 
 
 public class SubmissionFlowTestBase extends WebFlowTestCase {
 	
-	def authenticationManager=Holders.applicationContext.getBean("authenticationManager")
+	def grailsApplication = Holders.getGrailsApplication()
+    def fileSystemService = Holders.getApplicationContext().getBean("fileSystemService")
+    def modelService = Holders.getApplicationContext().getBean("modelService")
+    def authenticationManager=Holders.applicationContext.getBean("authenticationManager")
     def springSecurityService=Holders.applicationContext.getBean("springSecurityService")
-    def controller = new ModelController();
+    def controller = new ModelController()
 	
-	protected void clickCancelEndFlow() {
+	void tearDown() {
+        super.tearDown()
+        FileUtils.deleteDirectory(new File("target/vcs/git"))
+        FileUtils.deleteDirectory(new File("target/vcs/exchange"))
+    }
+    
+    void initialise() {
+        createUserAndRoles()
+        File exchangeDirectory = new File("target/vcs/exchange/")
+        exchangeDirectory.mkdirs()
+        grailsApplication.config.jummp.vcs.exchangeDirectory = "target/vcs/exchange/"
+        grailsApplication.config.jummp.vcs.workingDirectory = "target/vcs/git/"
+        File parentLocation = new File(grailsApplication.config.jummp.vcs.workingDirectory)
+        parentLocation.mkdirs()
+        fileSystemService.root = parentLocation
+        String containerPath = parentLocation.absolutePath + File.separator + "ttt"
+        fileSystemService.currentModelContainer = containerPath
+        modelService.vcsService.currentModelContainer = containerPath
+    }
+    
+    protected void testSetup() {
+        super.setUp()
+        mockRequest = new MockMultipartHttpServletRequest()
+        def mockGrailsRequest = new GrailsWebRequest(mockRequest, mockResponse,
+                mockServletContext, applicationContext)
+        mockGrailsRequest.setControllerName("model")
+        mockGrailsRequest.getRequest().setAttribute(
+                GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE,
+                "model")
+        mockGrailsRequest.getRequest().setAttribute(
+                GrailsApplicationAttributes.CONTROLLER,
+                controller)
+        RequestContextHolder.setRequestAttributes(mockGrailsRequest)
+        registerFlow("model/upload", controller.uploadFlow)
+        startFlow()
+        authenticateAsTestUser()
+    }
+    
+    void submitFileTest(String filepath,
+    					String format, 
+    					String mname, 
+    					String[] descriptionStrings) {
+        testSetup();
+        getToUploadPage()
+        signalEvent("Upload")
+        assertFlowState("uploadFiles")
+        final File MODEL_FILE = new File(filepath)
+        fileUploadPipeline(MODEL_FILE, format, mname, descriptionStrings);
+    }
+
+        protected void clickCancelEndFlow() {
         signalEvent("Cancel")
         assert "abort" == flowExecutionOutcome.id
     }
-   	
+
     /* Checks the current state against the supplied state id */
     protected void assertFlowState(String state) {
         assert state == flowExecution.activeSession.state.id
     }
-    
-    boolean createFlow = true;
-	 
-	def getFlow() {
-		 if (createFlow) {
-			 return controller.createFlow
-		 }
-		 return controller.updateFlow
-	 }
-    
-	protected void getToUploadPage() {
-    	assertFlowState("displayDisclaimer")
+
+    boolean createFlow = true
+
+    def getFlow() {
+        def flow = createFlow ? controller.createFlow : controller.updateFlow
+        flow.delegate = controller
+        flow
+    }
+
+    protected void getToUploadPage() {
+        assertFlowState("displayDisclaimer")
         signalEvent("Continue")
         assertFlowState("uploadFiles")
     }
-    
+
      /**
      * Sets the current authentication to testuser and does not model as admin user.
      * @return The testusers authentication
@@ -100,7 +157,7 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         modelAdminUser(false)
         return authenticate("testuser", "secret")
     }
-    
+
      /**
      * Sets and authentication based on username and password.
      * @param username The name of the user
@@ -109,13 +166,12 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
      */
     protected def authenticate(String username, String password) {
         def authToken = new UsernamePasswordAuthenticationToken(username, password)
-        System.out.println("Authenticating with "+username+" and "+password);
+        System.out.println("Authenticating with "+username+" and "+password)
         def auth = authenticationManager.authenticate(authToken)
         SecurityContextHolder.getContext().setAuthentication(auth)
         return auth
     }
-    
-      
+
     /**
      * Modifies the ifAnyGranted method of SpringSecurityUtils to return @p admin value.
      * @param admin if @c true, all access to ifAnyGranted returns @c true, @c false otherwise
@@ -125,14 +181,13 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
             return admin
         }
     }
-    
-    
+
     protected void ensureRoleExists(String _authority) {
         if (!Role.findByAuthority(_authority)) {
             new Role(authority: _authority).save()
         }
     }
-   
+
    /**
      * Creates three users and their roles:
      * @li testuser with password secret and role ROLE_USER
@@ -143,8 +198,8 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         User user, user2, admin, curator
         Person person
         if (!User.findByUsername("testuser")) {
-        	person=new Person(userRealName: "Test")
-        	user = new User(username: "testuser",
+            person=new Person(userRealName: "Test")
+            user = new User(username: "testuser",
                     password: springSecurityService.encodePassword("secret"),
                     person: person,
                     email: "test@test.com",
@@ -152,17 +207,17 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
                     accountExpired: false,
                     accountLocked: false,
                     passwordExpired: false)
-            assertNotNull(person.save(flush:true, failOnError:true)) 
+            assertNotNull(person.save(flush:true, failOnError:true))
             assertNotNull(user.save())
             assertNotNull(new AclSid(sid: user.username, principal: true).save(flush: true))
-            System.out.println("User created: "+user);
+            System.out.println("User created: "+user)
         } else {
             user = User.findByUsername("testuser")
-            System.out.println("User exists: "+user);
+            System.out.println("User exists: "+user)
         }
         if (!User.findByUsername("username")) {
             person=new Person(userRealName: "Test2")
-        	user2 = new User(username: "username",
+            user2 = new User(username: "username",
                     password: springSecurityService.encodePassword("verysecret"),
                     person: person,
                     email: "test2@test.com",
@@ -170,7 +225,7 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
                     accountExpired: false,
                     accountLocked: false,
                     passwordExpired: false)
-            assertNotNull(person.save(flush:true, failOnError:true)) 
+            assertNotNull(person.save(flush:true, failOnError:true))
             assertNotNull(user2.save())
             assertNotNull(new AclSid(sid: user2.username, principal: true).save(flush: true))
         } else {
@@ -178,7 +233,7 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         }
         if (!User.findByUsername("admin")) {
             person=new Person(userRealName: "administrator")
-        	admin = new User(username: "admin",
+            admin = new User(username: "admin",
                     password: springSecurityService.encodePassword("1234"),
                     person: person,
                     email: "admin@test.com",
@@ -194,7 +249,7 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         }
         if (!User.findByUsername("curator")) {
             person = new Person(userRealName: "Curator")
-        	assertNotNull(person.save(flush:true, failOnError:true)) 
+            assertNotNull(person.save(flush:true, failOnError:true))
             curator = new User(username: "curator",
                     password: springSecurityService.encodePassword("extremelysecret"),
                     person: person,
@@ -221,15 +276,13 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         Role curatorRole = Role.findByAuthority("ROLE_CURATOR")
         createUserRoleIfNeeded(curator, curatorRole)
     }
-    
+
     protected void createUserRoleIfNeeded(User user, Role role) {
-    	if (!UserRole.findByUserAndRole(user, role)) {
-    		UserRole.create(user, role, false)
-    	}
+        if (!UserRole.findByUserAndRole(user, role)) {
+            UserRole.create(user, role, false)
+        }
     }
 
-   
-    
     // Click through the upload pipeline with the supplied file and test name/description strings
     void fileUploadPipeline(File file, String format, String mname, String[] descriptionStrings) {
         Map<File, String> additionalFiles = getRandomAdditionalFiles(10)
@@ -281,14 +334,14 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
             }
         }
     }
-    
+
     public File getFileForTest(String filename, String text) {
         def tmp = System.getProperty("java.io.tmpdir")
         def testFile=new File(tmp + File.separator + filename)
         testFile.setText(text?: "")
         return testFile
     }
-    
+
     /*
      * Convenience function to create arbitrary additional files with corresponding
      * descriptions.
@@ -301,8 +354,8 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
         }
         return returnMe
     }
-    
-    /* 
+
+    /*
      * Convenience function to add the supplied main and additional files
      * to submission
      */
@@ -315,7 +368,7 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
             mockRequest.addParameter("description", additionalFiles.get(it))
         }
     }
-    
+
     /*
      * Convenience function to compare a map of String->byte[] retrieved from
      * the repository with the supplied list of files
@@ -333,17 +386,18 @@ public class SubmissionFlowTestBase extends WebFlowTestCase {
             assert savedFile == it.getBytes()
         }
     }
-    
-     /* 
-     * Adds the supplied file with parameters as a mock multipart file 
-     * 
+
+     /*
+     * Adds the supplied file with parameters as a mock multipart file
+     *
      */
     private void addFileToRequest(File modelFile, String formID, String contentType) {
         final file = new MockMultipartFile(formID, modelFile.getName(), contentType, modelFile.getBytes())
         mockRequest.addFile(file)
     }
-    
+
     private File bigModel() {
         return new File("test/files/BIOMD0000000272.xml")
     }
+
 }
