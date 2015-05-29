26e96aa3654a9349142e106488fd73bbfd65172a
Mihai Glonț <mglont@ebi.ac.uk>
2015-05-29 17:43:24 +0100

    whitespace

--- web-app/js/notification.js
+++ web-app/js/notification.js
@@ -1,37 +1,38 @@
-/* The notification system depends on including notificationDiv template in the 
-page (ensuring the presence of the notification div)
+/*
+ * The notification system depends on including notificationDiv template in the 
+ * page (ensuring the presence of the notification div)
  */
 function scheduleHide() {
-	setTimeout(function(){
-		$(".flashNotificationDiv").fadeOut("slow", function () {
-		$(".flashNotificationDiv").hide();
-	}); }, 4000);
-
+    setTimeout(function() {
+        $(".flashNotificationDiv").fadeOut("slow", function() {
+            $(".flashNotificationDiv").hide();
+        });
+    }, 4000);
 }
 
 function hideNow() {
-		$(".flashNotificationDiv").hide();
+    $(".flashNotificationDiv").hide();
 }
 
 function showNotification(message) {
-	$(".flashNotificationDiv").show()
-	$(".flashNotificationDiv").text(message)
+    $(".flashNotificationDiv").show()
+    $(".flashNotificationDiv").text(message)
 }
 
 function pollForNotifications(url) {
-	$.get( url, function( data ) {
-		if (data > 0) {
-			$("#notificationLink").text(' ('+data+')');
-			$("#notificationLink").show();
-		}
-		else {
-			$("#notificationLink").hide();
-		}
-	});
+    $.get( url, function(data) {
+        if (data > 0) {
+            $("#notificationLink").text(' (' + data + ')');
+            $("#notificationLink").show();
+        } else {
+            $("#notificationLink").hide();
+        }
+    });
 }
+
 function markAsRead(url, updateCount) {
-	$.get( url, function() {});
-	setTimeout(function() {
-			pollForNotifications(updateCount);
-	},1000);
+    $.get( url, function() {});
+    setTimeout(function() {
+        pollForNotifications(updateCount);
+    },1000);
 }
