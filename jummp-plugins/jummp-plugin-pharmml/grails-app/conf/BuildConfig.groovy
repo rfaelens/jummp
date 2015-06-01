8e4b63a83f0c20f94a78ff19713ca0a7a422507b
Mihai Glonț <mglont@ebi.ac.uk>
2015-06-01 23:58:02 +0100

    JUM-90 bring in latest libPharmML.

--- jummp-plugins/jummp-plugin-pharmml/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-pharmml/grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        compile("eu.ddmore.pharmml:libPharmML:0.4-beta-b3")
+        compile("eu.ddmore.pharmml:libPharmML:0.4-b1")
         runtime("commons-jexl:commons-jexl:1.1") { excludes 'junit', 'commons-logging' }
         compile "org.apache.tika:tika-core:1.3"
     }
