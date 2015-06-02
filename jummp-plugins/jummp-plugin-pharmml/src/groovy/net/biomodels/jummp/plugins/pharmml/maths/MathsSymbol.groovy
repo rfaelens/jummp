1f3c794ac63757ec0b997787c2db0293bf13625b
Mihai Glonț <mglont@ebi.ac.uk>
2015-06-02 18:32:08 +0100

    JUM-90 whitespace, formatting.

--- jummp-plugins/jummp-plugin-pharmml/src/groovy/net/biomodels/jummp/plugins/pharmml/maths/MathsSymbol.groovy
+++ jummp-plugins/jummp-plugin-pharmml/src/groovy/net/biomodels/jummp/plugins/pharmml/maths/MathsSymbol.groovy
@@ -1,22 +1,22 @@
 /**
-* Copyright (C) 2010-2014 EMBL-European Bioinformatics Institute (EMBL-EBI),
-* Deutsches Krebsforschungszentrum (DKFZ)
-*
-* This file is part of Jummp.
-*
-* Jummp is free software; you can redistribute it and/or modify it under the
-* terms of the GNU Affero General Public License as published by the Free
-* Software Foundation; either version 3 of the License, or (at your option) any
-* later version.
-*
-* Jummp is distributed in the hope that it will be useful, but WITHOUT ANY
-* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-* A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-* details.
-*
-* You should have received a copy of the GNU Affero General Public License along
-* with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
-**/
+ * Copyright (C) 2010-2014 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Deutsches Krebsforschungszentrum (DKFZ)
+ *
+ * This file is part of Jummp.
+ *
+ * Jummp is free software you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Jummp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with Jummp if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+ **/
 
 
 
@@ -25,16 +25,19 @@
 package net.biomodels.jummp.plugins.pharmml.maths
 
 class MathsSymbol {
-	public enum OutputFormat {MATHML, LATEX}
-	OutputFormat format=OutputFormat.MATHML;
-	String text;
-	String mapsTo;
-	
-	public MathsSymbol(String t, String m) {
-		text=t;
-		mapsTo=m;
-	}
-	
+    public enum OutputFormat {MATHML, LATEX}
+    OutputFormat format = OutputFormat.MATHML
+    String text
+    String mapsTo
+
+    public MathsSymbol(String t, String m) {
+        text = t
+        mapsTo = m
+    }
+
+    /**
+     * Convenience method that strips ".0" from integers
+     */
     private String simplifyNumber() {
         if (!mapsTo.equals("NaN")) {
             try {
@@ -47,15 +50,15 @@ class MathsSymbol {
         }
         return mapsTo
     }
-	
-	public String getMapping() {
-		if (format==OutputFormat.MATHML) {
-			return "<mi>${simplifyNumber()}</mi>"
-		}
-		return null
-	}
-	
-	public String toString() {
-		return mapsTo
-	}
+
+    public String getMapping() {
+        if (format == OutputFormat.MATHML) {
+            return "<mi>${simplifyNumber()}</mi>"
+        }
+        return null
+    }
+
+    public String toString() {
+        return mapsTo
+    }
 }
