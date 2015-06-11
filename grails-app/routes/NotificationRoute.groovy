e0f86c443ad29c799901a9d28bb1b81fd7160581
Raza <raza.ali@ebi.ac.uk>
2015-06-11 10:38:10 +0100

    Remove unused variable

--- grails-app/routes/NotificationRoute.groovy
+++ grails-app/routes/NotificationRoute.groovy
@@ -4,14 +4,10 @@ class NotificationRoute extends RouteBuilder {
 
     @Override
     void configure() {
-        def config = grailsApplication?.config
-
-        // example:
         from("seda:model.publish").to("bean:notificationService?method=modelPublished")
     	from("seda:model.readAccessGranted").to("bean:notificationService?method=readAccessGranted")
     	from("seda:model.writeAccessGranted").to("bean:notificationService?method=writeAccessGranted")
         from("seda:model.delete").to("bean:notificationService?method=delete")
         from("seda:model.update").to("bean:notificationService?method=update")
-    	
     }
 }
