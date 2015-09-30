5978fdd7641037c07646cdf41076a0626426c961
Mihai Glonț <mglont@ebi.ac.uk>
2015-10-01 00:54:15 +0100

    bring in updated domain classes.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -79,7 +79,8 @@ grails.project.dependency.resolution = {
         compile "com.googlecode.multithreadedtc:multithreadedtc:1.01"
         runtime 'mysql:mysql-connector-java:5.1.17'
         runtime "postgresql:postgresql:9.1-901.jdbc4"
-        compile "net.biomodels.jummp:AnnotationStore:0.2.1"
+        compile "net.biomodels.jummp:AnnotationStore:0.2.2"
+
         compile("org.apache.solr:solr-solrj:4.10.1") {
             excludes 'wstx-asl', //a newer version of woodstox comes with jsbml
                 // httpcomponents 4.3.1 is incompatible with 4.2, which breaks jena-arq
