e04b675ee5c87d80da597b1d322073b937c105ca
Mihai Glonț <mglont@ebi.ac.uk>
2015-11-26 15:55:14 +0000

    Use the latest version of the externalised domains.

--- jummp-plugins/jummp-plugin-annotation-source-ddmore/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-annotation-source-ddmore/grails-app/conf/BuildConfig.groovy
@@ -66,7 +66,7 @@ grails.project.dependency.resolution = {
             excludes 'spring-core', 'spring-context'
         }
 
-        compile "net.biomodels.jummp:AnnotationStore:0.2.2"
+        compile "net.biomodels.jummp:AnnotationStore:0.2.5"
         compile("net.sf.jung:jung-graph-impl:$jungVersion")
 
         // useful for WordUtils.capitalise()
