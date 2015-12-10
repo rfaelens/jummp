60d0a418e39433f82e6638c178e5decc38396e26
tnguyenv <nvntung@gmail.com>
2015-12-10 19:46:22 +0000

    Revert "config the registration link in the login area based on the allowance of anonymous registration"
    This reverts commit b4b1e40f22081d1b3c18ebd0ce201575299594db.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -65,7 +65,7 @@ class UrlMappings {
         "/archive"(controller: "search", action: "archive")
         "/feedback"(controller:"jummp", action:"feedback")
         "/lookupUser"(controller:"jummp", action:"lookupUser")
-        //"/registration"(controller:"usermanagement", action:"create")
+        "/registration"(controller:"usermanagement", action:"create")
         "/forgotpassword"(controller:"usermanagement", action:"forgot")
         "/user/editUser"(controller:"usermanagement", action:"edit")
         "/user/editPassword"(controller:"usermanagement", action:"editPassword")
