f28a758e12817228996d562dc610081991ff73de
Mihai Glonț <mglont@ebi.ac.uk>
2016-01-14 17:20:19 +0000

    JUM-169 clear annotations prior to reindexing.
    Thus we avoid annotations being duplicated as part of the reindexing
    process.
    
    Fixes JUM-169.

--- test/integration/net/biomodels/jummp/core/SearchTests.groovy
+++ test/integration/net/biomodels/jummp/core/SearchTests.groovy
@@ -32,13 +32,22 @@ package net.biomodels.jummp.core
 
 import grails.test.mixin.TestMixin
 import grails.test.mixin.integration.IntegrationTestMixin
+import net.biomodels.jummp.annotationstore.ElementAnnotation
+import net.biomodels.jummp.annotationstore.Qualifier
+import net.biomodels.jummp.annotationstore.ResourceReference
+import net.biomodels.jummp.annotationstore.Statement
+import net.biomodels.jummp.core.adapters.DomainAdapter
 import net.biomodels.jummp.core.model.ModelFormatTransportCommand
+import net.biomodels.jummp.core.model.ModelState
 import net.biomodels.jummp.core.model.ModelTransportCommand
 import net.biomodels.jummp.core.model.RepositoryFileTransportCommand
+import net.biomodels.jummp.core.model.ValidationState
 import net.biomodels.jummp.model.Model
+import net.biomodels.jummp.model.ModelFormat
 import net.biomodels.jummp.model.Revision
-import net.biomodels.jummp.core.adapters.DomainAdapter
+import net.biomodels.jummp.model.RepositoryFile
 import net.biomodels.jummp.plugins.git.GitManagerFactory
+import net.biomodels.jummp.plugins.security.User
 import org.apache.commons.io.FileUtils
 import org.junit.*
 import static org.junit.Assert.*
@@ -89,6 +98,50 @@ class SearchTests extends JummpIntegrationTest {
         result = searchForModel("pharmml")
         assertNotNull result
         assertEquals(upped.id, result.id)
+    }
+
+    @Test
+    void testIndexRegenerationResetsTheDatabaseAnnotations() {
+        // given a model revision with one annotation
+        def model = new Model(submissionId: 'MODEL123', vcsIdentifier: 'aaa/model1')
+        def rev = new Revision(name: 'model v1', description: 'sample model snapshot', comment:
+                'please import me', uploadDate: new Date(), validated: true, owner: User.first(),
+                revisionNumber: 1, validationState: ValidationState.APPROVE, format:
+                ModelFormat.first(), minorRevision: false, vcsId: 'sha256hash', model: model)
+        def rf = new RepositoryFile(path: '/nowhere', mainFile: true, description: 'not found',
+                mimeType: 'text/plain', hidden: false, userSubmitted: true)
+        rev.addToRepoFiles rf
+        model.addToRevisions(rev)
+        assertNotNull model.save()
+        assertFalse model.hasErrors()
+
+        def p = new Qualifier(accession: 'hasName', uri: 'http://example.com/hasName',
+                qualifierType: 'UNKNOWN')
+        assertNotNull p.save()
+        assertFalse p.hasErrors()
+
+        def o = new ResourceReference(datatype: 'foo', collectionName: 'bar', name: 'baz', uri:
+                'http://ddg.gg/bar/baz', accession: 'baz', shortName: 'baz')
+        assertNotNull o.save()
+        assertFalse o.hasErrors()
+
+        def stmt = new Statement(subjectId: 'someone', qualifier: p, object: o, )
+        def ea   = new ElementAnnotation(statement: stmt, creatorId: 'notMe', revision: rev)
+        assertNotNull ea.save()
+        assertFalse ea.hasErrors()
+        assertEquals 1, Statement.count()
+        assertEquals 1, ElementAnnotation.count()
+        assertEquals 1, ResourceReference.count()
+        assertEquals 1, Qualifier.count()
+
+        // when we delete all element annotations and statements before re-indexing
+        searchService.clearAnnotationStatementsFromDatabase()
+
+        // we start with a clean database (the qualifiers and xrefs are preserved)
+        assertEquals 0, ElementAnnotation.count()
+        assertEquals 0, Statement.count()
+        assertEquals 1, Qualifier.count()
+        assertEquals 1, ResourceReference.count()
     }
 
     @Before
