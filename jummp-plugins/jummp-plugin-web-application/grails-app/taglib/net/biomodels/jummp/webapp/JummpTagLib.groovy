140d05867e6b6d679cc7a55467617dfb0c32aca9
Tung NGUYEN <nvntung@gmail.com>
2016-05-11 10:35:05 +0100

    Leave the description of additional file empty
    In the case of no description, it should let it be empty. To do that, we
    need to clarify this field before displaying it. Of course, the
    description should be set empty string (i.e. "" or '') in the map of the
    additional files in working.

--- jummp-plugins/jummp-plugin-web-application/grails-app/taglib/net/biomodels/jummp/webapp/JummpTagLib.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/taglib/net/biomodels/jummp/webapp/JummpTagLib.groovy
@@ -79,7 +79,7 @@ class JummpTagLib {
             out << "<input style='display:none' type='file' id='additionalFilesExisting' " +
                    "name='additionalFilesExisting' value='${name}'></td>\n\t"
             out << "<td style='width: 285px'>" +
-                   "<input name='description${counter}' id='description${counter}' type='text' value='${command.description}' " +
+                   "<input name='description${counter}' id='description${counter}' type='text' value='${command.description ?: ""}' " +
                    "style='width: 100%; box-sizing: border-box; -webkit-box-sizing: border-box; -moz-box-sizing: border-box;'></td>\n\t"
             out << "<td><a href='#' class='killer' title='Discard file'>Discard</a></td>\n"
             out << "</tr>\n"
@@ -95,7 +95,7 @@ class JummpTagLib {
         attrs.additionalsOnUI.each { f ->
             RepositoryFileTransportCommand command = f as RepositoryFileTransportCommand
             String name = new File(command.path).name
-            result.append(name).append(" : ").append(command.description).append(", ")
+            result.append(name).append(" : ").append(command.description ?: "").append(", ")
         }
         if (result.length() > 0) {
             result.setLength(result.length() - 2)
